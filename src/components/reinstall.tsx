d5989cec231826acc53d29de31997d534832e5ce
Tems <tems@tems.pl>
2025-07-02 15:27:27 +0200

    add shop ID input validation and update reinstall logic

--- src/components/reinstall.tsx
+++ src/components/reinstall.tsx
@@ -5,18 +5,23 @@ import {useState} from "react";
 const Reinstall = (props: { shop: { "domain": string, "date": string, id: number }, types: string[] }) => {
     const {shop, types} = props
     const [selectedType, setSelectedType] = useState<string>(types[0])
+    const [shopId, setShopId] = useState<number>(0)
     const token = localStorage.getItem("token");
     const reinstall = () => {
         console.log(selectedType, shop.id)
+        if (shopId == 0) {
+            alert("Podaj ID sklepu!")
+            return
+        }
         axios.post(backendUrl + "shop/" + shop.id + "/reinstall", {
-            type: selectedType,
-            id: shop.id
+            "type": selectedType != undefined ? selectedType : types[0],
+            id: shopId
         }, {
             headers: {
                 "Authorization": "Bearer " + token
             }
         }).then(r => {
-            alert(r.data.msg)
+            alert(r.data.message)
         })
     }
 
@@ -35,7 +40,7 @@ const Reinstall = (props: { shop: { "domain": string, "date": string, id: number
                 ))}
             </select>
             <p className="mt-2">ID sklepu na vishop.pl</p>
-            <input type="number" id="reinstall_id" name="id" min="1"
+            <input type="number" id="reinstall_id" name="id" min="1" value={shopId} onChange={e => setShopId(Number(e.target.value))}
                    className="mt-1.5 rounded-lg border-gray-300 text-gray-700 sm:text-sm p-2 w-full bg-gray-50"/><br/>
             <input type="button" id="reinstall" value="Reinstaluj szablon" onClick={reinstall}
                    className="mt-3 p-3 rounded border border-red-400 font-medium text-black hover:bg-red-300"/>
