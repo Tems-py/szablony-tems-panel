f17078600b8784233c5fa38d6df9a138308ba6f7
Tems <tems@tems.pl>
2025-11-13 20:06:37 +0100

    add archive button

--- src/routes/shop.tsx
+++ src/routes/shop.tsx
@@ -43,6 +43,31 @@ function Shop(props: { shop: { "domain": string, "date": string, id: number, dis
         )
     }
 
+    const downloadZip = () => {
+        axios({
+            url: backendUrl + "shop/" + shop.id + "/archive",
+            method: 'POST',
+            responseType: 'blob', // important for binary data
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        })
+            .then((response) => {
+                // const blob = new Blob([response.data], {type: 'application/zip'});
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', `${shop.id}_backup.zip`); // filename
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+                window.URL.revokeObjectURL(url);
+            })
+            .catch((error) => {
+                console.error('Nie udało się pobrać:', error);
+            });
+    }
+
     useEffect(() => {
         getShopData()
     }, [])
@@ -50,7 +75,8 @@ function Shop(props: { shop: { "domain": string, "date": string, id: number, dis
     return (
         <div className="w-full">
             <div className="h-full w-full rounded-lg bg-gray-100 p-3 lg:p-10 grid grid-cols-1 md:grid-cols-4 gap-4">
-                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6 flex flex-col gap-4 md:col-span-3">
+                <section
+                    className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6 flex flex-col gap-4 md:col-span-3">
                     <h1 className="font-bold text-3xl">{shop.domain} <span
                         className="text-gray-600 text-xl mx-2 p-2 bg-slate-300 rounded">id: {shop.id}</span></h1>
                     <p>Twój pakiet jest aktywny do <strong className="font-bold">{shop.date}</strong></p>
@@ -72,19 +98,31 @@ function Shop(props: { shop: { "domain": string, "date": string, id: number, dis
                                                               target="_blank"
                                                               className="text-indigo-500 font-bold underline">{shop.id}.tems.pl</a>.
                         Tej domeny nie można zmienić</p>
-                    <Link className="justify-self-end w-fit px-4 text-center inline-block rounded border border-indigo-600 bg-indigo-600 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-                       to={"/shop/" + shop.id + "/domain"}>Ustawienia domeny</Link>
+                    <Link
+                        className="justify-self-end w-fit px-4 text-center inline-block rounded border border-indigo-600 bg-indigo-600 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                        to={"/shop/" + shop.id + "/domain"}>Ustawienia domeny</Link>
                 </section>
 
                 <section className="bg-white rounded-lg shadow-sm border border-gray-200 px-6 py-3 flex flex-col gap-3">
                     <strong className="block font-xl">Logi</strong>
-                    <p className="text-md">Tutaj sprawdzisz czy szablon uruchomił się poprawnie oraz znajdziesz ewentualne błędy uruchamiania</p>
+                    <p className="text-md">Tutaj sprawdzisz czy szablon uruchomił się poprawnie oraz znajdziesz
+                        ewentualne błędy uruchamiania</p>
                     <a href={"/shop/" + shop.id + "/logs"}
                        className="w-fit mt-4 px-4 text-center inline-block rounded border border-indigo-600 bg-indigo-600 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500">Logi</a>
 
                 </section>
 
-                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6 flex flex-col gap-3">
+                <section
+                    className="bg-white rounded-lg shadow-sm border border-gray-200 px-6 py-3 flex flex-col gap-3">
+                    <strong className="block font-xl">Backup</strong>
+                    <p>
+                        Pobierz backup sklepu:<br/>
+                        <button
+                            className="mt-4 px-4 text-center inline-block rounded border border-indigo-600 bg-indigo-600 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                            onClick={downloadZip}
+                            id="pay">Pobierz
+                        </button>
+                    </p>
                     {/*<strong className="block font-xl">Zmiana wersji Node.js <span className="text-gray-500 text-sm">domyślnie 14</span></strong>*/}
 
                     {/*<strong className="block font-xl">Użycie</strong>*/}
@@ -93,11 +131,14 @@ function Shop(props: { shop: { "domain": string, "date": string, id: number, dis
                     {/*<StatsBar progress={shop.diskUsage * 100} name="CPU"/>*/}
                 </section>
 
-                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6"><Reinstall shop={shop}
-                                                                                                             types={types}/>
+                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6"><Reinstall
+                    shop={shop}
+                    types={types}/>
                 </section>
-                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6 md:col-span-2"><AdminList/></section>
-                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6"><Restart shop={shop}/> </section>
+                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6 md:col-span-2">
+                    <AdminList/></section>
+                <section className="bg-white rounded-lg shadow-sm border border-gray-200 p-3 md:p-6"><Restart
+                    shop={shop}/></section>
             </div>
         </div>
     )
